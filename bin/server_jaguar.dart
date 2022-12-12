412c288d08eb32bf46e3c33db5e11c7de561f74e
Yoshitsugu Tahara <arisahyper0000@gmail.com>
2022-12-13 02:59:06 +0900

    feat: init

---
+++ bin/server_jaguar.dart
@@ -0,0 +1,5 @@
+import 'package:server_jaguar/server_jaguar.dart' as server_jaguar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${server_jaguar.calculate()}!');
+}
---
+++ test/server_jaguar_test.dart
@@ -0,0 +1,8 @@
+import 'package:server_jaguar/server_jaguar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
