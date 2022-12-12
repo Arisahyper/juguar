a0d60bf9efd3d78ba5c8badf50f8b572cf2c870d
Yoshitsugu Tahara <arisahyper0000@gmail.com>
2022-12-13 03:08:03 +0900

    feat: routing

--- bin/server_jaguar.dart
+++ bin/server_jaguar.dart
@@ -1,5 +1,66 @@
-import 'package:server_jaguar/server_jaguar.dart' as server_jaguar;
+import 'dart:async' show Future;
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${server_jaguar.calculate()}!');
+class Service {
+  Handler get handler {
+    final router = Router();
+
+    router.get('/', (Request request) {
+      return Response.ok('Get request');
+    });
+
+    router.get('/<id>', (Request request, String id) {
+      return Response.ok('path parameter $id');
+    });
+
+    router.post('/', (Request request) async {
+      final body = await request.readAsString();
+      final params = Uri.splitQueryString(body);
+      return Response.ok(params['msg']);
+    });
+
+    router.patch('/', (Request request) {
+      return Response.ok('patch request');
+    });
+
+    router.put('/', (Request request) {
+      return Response.ok('put request');
+    });
+
+    router.get('/delay', (Request request) async {
+      await Future.delayed(Duration(milliseconds: 100));
+      return Response.ok('_o/');
+    });
+
+    router.mount('/api/', Api().router);
+
+    router.all('/<ignored|.*>', (Request request) {
+      return Response.notFound('Page not found');
+    });
+
+    return router;
+  }
+}
+
+class Api {
+  Future<Response> _messages(Request request) async {
+    return Response.ok('[]');
+  }
+
+  Router get router {
+    final router = Router();
+    router.get('/messages', _messages);
+    router.get('/messages/', _messages);
+    router.all('/<ignored|.*>', (Request request) => Response.notFound('null'));
+
+    return router;
+  }
+}
+
+void main() async {
+  final service = Service();
+  final server = await shelf_io.serve(service.handler, 'localhost', 8080);
+  print('Server running on localhost:${server.port}');
 }
--- test/server_jaguar_test.dart
+++ test/server_jaguar_test.dart
@@ -1,8 +1,1 @@
-import 'package:server_jaguar/server_jaguar.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
